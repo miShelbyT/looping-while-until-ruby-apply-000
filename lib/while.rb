3b191a4679f5ad8a0770c00c1ea2eaae4344eabd
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-08 16:12:20 +0000

    Done.

--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,9 @@
 def using_while
   levitation_force = 6
-	
-  #your code here
+    while levitation_force < 10
+      puts phrase = "Wingardium Leviosa"
+      levitation_force += 1
+    end
 end
 
-
+using_while
